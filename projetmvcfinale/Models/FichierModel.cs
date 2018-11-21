25337dd7a27a69ee72c2151b865ab9dda4f24f65
Olivier <0991038@csimple.org>
2018-11-21 14:49:19 -0500

    Commencer l'ajout de fichier

---
+++ projetmvcfinale/Models/FichierModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetmvcfinale.Models
+{
+    public class FichierModel
+    {
+        [DataType(DataType.Upload)]
+        [Display(Name = "Fichier")]
+        [Required(ErrorMessage = "Veuillez sélectionner un fichier a téléverser")]
+        public string Fichier { get; set; }
+    }
+}
