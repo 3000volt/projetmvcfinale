c960e64d0c1da1f72b573614b86d2d97bc0d872d
Félix Lagacé <1471667@cegepoutaouais.qc.ca>
2018-12-14 20:17:38 -0500

    gestion des infos terminé

---
+++ projetmvcfinale/Models/SousCategorieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetmvcfinale.Models
+{
+    public class SousCategorieViewModel
+    {
+        public int IdSousCategorie { get; set; }
+        public string NomSousCategorie { get; set; }
+        public string NomCategorie { get; set; }
+    }
+}
